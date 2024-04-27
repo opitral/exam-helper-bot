8b5391fc1cd3cd7837feaf16169498aba710af37
Володимир Слободян <opitral@proton.me>
2024-04-27 17:28:36 +0300

    add exception handlers

--- bot.py
+++ bot.py
@@ -13,54 +13,74 @@ tickets = db["tickets"]
 
 @bot.message_handler(commands=["start"])
 def start_message(message):
-    if str(message.chat.id) == getenv("TELEGRAM_USER_ID"):
-        topics_kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        for topic in list(topics.find()):
-            topics_kb.add(types.KeyboardButton(topic["name"]))
+    try:
+        if str(message.chat.id) == getenv("TELEGRAM_USER_ID"):
+            topics_kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            for topic in list(topics.find()):
+                topics_kb.add(types.KeyboardButton(topic["name"]))
 
-        bot.send_message(message.chat.id, "Твоя роль: *користувач*", parse_mode="Markdown", reply_markup=topics_kb)
+            bot.send_message(message.chat.id, "Твоя роль: *користувач*", parse_mode="Markdown", reply_markup=topics_kb)
 
-    elif str(message.chat.id) == getenv("TELEGRAM_HELPER_ID"):
-        bot.send_message(message.chat.id, "Твоя роль: *помічник*", parse_mode="Markdown")
+        elif str(message.chat.id) == getenv("TELEGRAM_HELPER_ID"):
+            bot.send_message(message.chat.id, "Твоя роль: *помічник*", parse_mode="Markdown")
+
+    except Exception as ex:
+        print(ex)
+        bot.send_message(message.chat.id, "упс(")
 
 
 @bot.message_handler(content_types=["text"])
 def text_message(message):
-    if str(message.chat.id) == getenv("TELEGRAM_USER_ID"):
-        topic = topics.find_one({"name": message.text})
+    try:
+        if str(message.chat.id) == getenv("TELEGRAM_USER_ID"):
+            topic = topics.find_one({"name": message.text})
 
-        if topic:
-            tickets_kb = types.InlineKeyboardMarkup()
-            for ticket in list(tickets.find({"topic_number": topic["number"]})):
-                tickets_kb.add(types.InlineKeyboardButton(f"#{ticket['number']} {ticket['question']}", callback_data=f"ticket-{ticket['number']}"))
+            if topic:
+                tickets_kb = types.InlineKeyboardMarkup()
+                for ticket in list(tickets.find({"topic_number": topic["number"]})):
+                    tickets_kb.add(types.InlineKeyboardButton(f"#{ticket['number']} {ticket['question']}", callback_data=f"ticket-{ticket['number']}"))
 
-            bot.send_message(message.chat.id, f"Тема: *{topic['name']}*", parse_mode="Markdown", reply_markup=tickets_kb)
+                bot.send_message(message.chat.id, f"Тема: *{topic['name']}*", parse_mode="Markdown", reply_markup=tickets_kb)
 
-        else:
-            bot.send_message(message.chat.id, "Тему не знайдено", parse_mode="Markdown")
+            else:
+                bot.send_message(message.chat.id, "Тему не знайдено", parse_mode="Markdown")
+
+    except Exception as ex:
+        print(ex)
+        bot.send_message(message.chat.id, "упс(")
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("ticket"))
 def ticket_message(call):
-    if str(call.message.chat.id) == getenv("TELEGRAM_USER_ID"):
-        ticket_number = int(call.data.split("-")[1])
-        ticket = tickets.find_one({"number": ticket_number})
-        topic = topics.find_one({"number": ticket["topic_number"]})
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f"Тема: *{topic['name']}*\nНомер білету: *{ticket['number']}*\nПитання: *{ticket['question']}*\n\n{ticket['answer']}", reply_markup=types.InlineKeyboardMarkup().add(types.InlineKeyboardButton("Надіслати помічнику", callback_data=f"help-{ticket['number']}")), parse_mode="Markdown")
+    try:
+        if str(call.message.chat.id) == getenv("TELEGRAM_USER_ID"):
+            ticket_number = int(call.data.split("-")[1])
+            ticket = tickets.find_one({"number": ticket_number})
+            topic = topics.find_one({"number": ticket["topic_number"]})
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f"Тема: *{topic['name']}*\nНомер білету: *{ticket['number']}*\nПитання: *{ticket['question']}*\n\n{ticket['answer']}", reply_markup=types.InlineKeyboardMarkup().add(types.InlineKeyboardButton("Надіслати помічнику", callback_data=f"help-{ticket['number']}")), parse_mode="Markdown")
+
+    except Exception as ex:
+        print(ex)
+        bot.answer_callback_query(callback_query_id=call.id, text="упс(")
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("help"))
 def help_message(call):
-    if str(call.message.chat.id) == getenv("TELEGRAM_USER_ID"):
-        ticket_number = int(call.data.split("-")[1])
-        ticket = tickets.find_one({"number": ticket_number})
-        topic = topics.find_one({"number": ticket["topic_number"]})
-        bot.send_message(getenv("TELEGRAM_HELPER_ID"), f"Тема: *{topic['name']}*\nНомер білету: *{ticket['number']}*\nПитання: *{ticket['question']}*\n\n{ticket['answer']}", parse_mode="Markdown")
-        bot.edit_message_text(chat_id=call.message.chat.id,
-                              message_id=call.message.message_id,
-                              text=f"Тема: *{topic['name']}*\nНомер білету: *{ticket['number']}*\nПитання: *{ticket['question']}*\n\n{ticket['answer']}",
-                              parse_mode="Markdown")
-        bot.answer_callback_query(callback_query_id=call.id, text="Білет надіслано")
+    try:
+        if str(call.message.chat.id) == getenv("TELEGRAM_USER_ID"):
+            ticket_number = int(call.data.split("-")[1])
+            ticket = tickets.find_one({"number": ticket_number})
+            topic = topics.find_one({"number": ticket["topic_number"]})
+            bot.send_message(getenv("TELEGRAM_HELPER_ID"), f"Тема: *{topic['name']}*\nНомер білету: *{ticket['number']}*\nПитання: *{ticket['question']}*\n\n{ticket['answer']}", parse_mode="Markdown")
+            bot.edit_message_text(chat_id=call.message.chat.id,
+                                  message_id=call.message.message_id,
+                                  text=f"Тема: *{topic['name']}*\nНомер білету: *{ticket['number']}*\nПитання: *{ticket['question']}*\n\n{ticket['answer']}",
+                                  parse_mode="Markdown")
+            bot.answer_callback_query(callback_query_id=call.id, text="Білет надіслано")
+
+    except Exception as ex:
+        print(ex)
+        bot.answer_callback_query(callback_query_id=call.id, text="упс(")
 
 
 if __name__ == "__main__":
